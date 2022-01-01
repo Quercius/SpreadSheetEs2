dbe39fd511dec68aebdac522630f643e5d2ce082
Tommaso Querci <tommiquerci@gmail.com>
2022-01-01 12:03:42 +0100

    Creata classe Min

---
+++ Min.cpp
@@ -0,0 +1,17 @@
+//
+// Created by tommi on 01/01/2022.
+//
+
+#include "Min.h"
+
+void Min::compute() {
+    min = std::begin(opValues)->getValue();
+    for (auto itr = std::begin(opValues); itr != std::end(opValues); itr++) {
+        if (itr->getValue() < min)
+            min = itr->getValue();
+    }
+}
+
+int Min::getMin() const {
+    return min;
+}
---
+++ Min.h
@@ -0,0 +1,25 @@
+//
+// Created by tommi on 01/01/2022.
+//
+
+#ifndef SPREADSHEETES2_MIN_H
+#define SPREADSHEETES2_MIN_H
+
+#include "Operation.h"
+
+class Min : public Operation {
+public:
+    Min(Values *subj) : Operation(subj) {
+        compute();
+    }
+
+    void compute() override;
+
+    int getMin() const;
+
+private:
+    int min;
+};
+
+
+#endif //SPREADSHEETES2_MIN_H
